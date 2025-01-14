592de94e8c1888155bfae876fac92a4b0aff15e7
Rohit Arya <rohit1447481@gmail.com>
2025-01-14 20:50:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,20 +6,20 @@ const { connectDB } = require("./config/db");
 const bodyParser = require("body-parser");
 const AuthRouter = require("./Routes/auth");
 dotenv.config({ path: "./config/config.env" });
-const corsOptions = { 
-  origin: 'https://expense-tracker-phi-lake.vercel.app/',  
-  methods: {"GET","POST","PUT","DELETE"},
-  credentials: true,
-}
+
 connectDB();
 const app = express();
 const PORT = 8000
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+app.use(cors( { 
+  origin: 'https://expense-tracker-phi-lake.vercel.app/',  
+  methods: {"GET","POST","PUT","DELETE"},
+  credentials: true,
+}));
 app.use("/auth", AuthRouter);
 app.use("/epxense", ExpanseRouter);
 app.get("/",(req,res)=>{
-  res.json({msg:"SERVER IS READY TO USE"})
+  res.json({msg:"SERVER IS READY TO USE With changes"})
 })
 app.listen(PORT, () => {
   console.log(`Server Started ${PORT}`);
