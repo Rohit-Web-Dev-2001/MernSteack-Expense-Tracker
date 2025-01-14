b48a47b6bec2ed6ccb015100333614c90c9f5650
Rohit Arya <rohit1447481@gmail.com>
2025-01-14 20:53:16 +0530

    Reversing changes index.js

--- server/index.js
+++ server/index.js
@@ -11,11 +11,7 @@ connectDB();
 const app = express();
 const PORT = 8000
 app.use(bodyParser.json());
-app.use(cors( { 
-  origin: 'https://expense-tracker-phi-lake.vercel.app/',  
-  methods: {"GET","POST","PUT","DELETE"},
-  credentials: true,
-}));
+app.use(cors());
 app.use("/auth", AuthRouter);
 app.use("/epxense", ExpanseRouter);
 app.get("/",(req,res)=>{
